0ab0bd13e41be3f36654032e74092000851b1c9b
nyulg <g.nyul94@gmail.com>
2017-08-01 21:09:00 +0200

    update

---
+++ app/src/main/java/hu/bitnet/smartparking/Fragments/Home.java
@@ -0,0 +1,34 @@
+package hu.bitnet.smartparking.Fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import hu.bitnet.smartparking.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Home extends Fragment {
+
+
+    public Home() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View home = inflater.inflate(R.layout.fragment_home, container, false);
+        TextView appbartext = (TextView) getActivity().findViewById(R.id.appbar_text);
+        appbartext.setText("Find a parking");
+        return home;
+    }
+
+}
